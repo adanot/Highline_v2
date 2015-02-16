97094a4c67cf7c350561fe08738b72c39106608e
2Deshka <likezsmj@gmail.com>
2015-02-16 16:48:05 +0200

    Release app

--- app/src/main/java/com/example/anottingham/highline/MainActivity.java
+++ app/src/main/java/com/example/anottingham/highline/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.anottingham.highline;
 
 import android.app.Activity;
+import android.content.Context;
 import android.hardware.Sensor;
 import android.hardware.SensorEvent;
 import android.hardware.SensorEventListener;
@@ -9,48 +10,62 @@ import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.Button;
-import android.widget.TextView;
+import android.widget.FrameLayout;
 import android.widget.Toast;
-
-import com.example.anottingham.highline.util.SystemUiHider;
 
 
 /**
  * An example full-screen activity that shows and hides the system UI (i.e.
  * status bar and navigation/system bar) with user interaction.
  *
- * @see SystemUiHider
+ * @see com.example.anottingham.highline.util.SystemUiHider
  */
-public class MainActivity extends Activity {
+public class MainActivity extends Activity implements View.OnClickListener, SensorEventListener{
 
-    //private static final String LOGTAG = "HighLine";
-    Button exit_button, settings_button;
-    MediaPlayer NorthPlayer, EastPlayer, SouthPlayer, WestPlayer;
-    SensorManager mSensorManager;
-    Sensor mAccelerometer;
-    Float ALPHA = 0.03f;
-    Float azimuth = null;
-    Float pi = (float)Math.PI;
+    private CompassView compassView;
 
-    TextView northText = (TextView) findViewById(R.id.northView);
-    TextView eastText = (TextView) findViewById(R.id.eastview);
-    TextView southText = (TextView) findViewById(R.id.southView);
-    TextView westText = (TextView) findViewById(R.id.westView);
+    private MediaPlayer NorthPlayer, EastPlayer, SouthPlayer, WestPlayer;
+    private static SensorManager sensorService;
+    private Sensor sensor;
+
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-
         setContentView(R.layout.activity_main);
 
-        // Sensor(compass) setup
-        mSensorManager = (SensorManager)getSystemService(SENSOR_SERVICE);
-        mAccelerometer = mSensorManager.getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        //initialize view components
+        compassView = new CompassView(this);
+        FrameLayout frameLayout = (FrameLayout) findViewById(R.id.frame_layout);
+        frameLayout.addView(compassView, new ViewGroup.LayoutParams(
+                        ViewGroup.LayoutParams.WRAP_CONTENT,
+                        ViewGroup.LayoutParams.WRAP_CONTENT)
+        );
 
-        if (mAccelerometer != null) {
-            mSensorManager.registerListener(mySensorEventListener, mAccelerometer,
+        Button exit_button = (Button) findViewById(R.id.exit_button);
+        exit_button.setOnClickListener(this);
+
+        Button settings_button = (Button) findViewById(R.id.settings_button);
+        settings_button.setOnClickListener(this);
+
+		/* Initialize MediaPlayers */
+        NorthPlayer = MediaPlayer.create(this, R.raw.north);
+        EastPlayer = MediaPlayer.create(this, R.raw.east);
+        SouthPlayer = MediaPlayer.create(this, R.raw.south);
+        WestPlayer = MediaPlayer.create(this, R.raw.west);
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+
+        sensorService = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        sensor = sensorService.getDefaultSensor(Sensor.TYPE_ORIENTATION);
+        if (sensor != null) {
+            sensorService.registerListener(this, sensor,
                     SensorManager.SENSOR_DELAY_NORMAL);
             Log.i("Compass MainActivity", "Registered for ORIENTATION Sensor");
         } else {
@@ -60,40 +75,26 @@ public class MainActivity extends Activity {
             finish();
         }
 
-
-        exit_button = (Button) findViewById(R.id.exit_button);
-        exit_button.setOnClickListener((android.view.View.OnClickListener) this);
-
-        settings_button = (Button) findViewById(R.id.settings_button);
-        settings_button.setOnClickListener((android.view.View.OnClickListener) this);
-
-		/* Initialize MediaPlayers */
-        NorthPlayer = MediaPlayer.create(this, R.raw.north);
-        EastPlayer = MediaPlayer.create(this, R.raw.east);
-        SouthPlayer = MediaPlayer.create(this, R.raw.south);
-        WestPlayer = MediaPlayer.create(this, R.raw.west);
-
-        NorthPlayer.start();
-        EastPlayer.start();
-        SouthPlayer.start();
-        WestPlayer.start();
+        startMediaPlayers();
     }
 
-
-
-    /* Method to control button interactions */
-    public void onClick(View view){
-        switch (view.getId()){
-            case R.id.settings_button:
-                //startSettingsActivity();
-                break;
-
-            case R.id.exit_button:
-                android.os.Process.killProcess(android.os.Process.myPid());
-                break;
-
+    @Override
+    protected void onPause() {
+        super.onPause();
+        //to stop the listener and save battery
+        if (sensor != null) {
+            sensorService.unregisterListener(this);
         }
     }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        releaseMediaResources();
+    }
+
+    /* Method to control button interactions */
+
     //TODO: Create settings class
     /*
     public void startSettingsActivity(){
@@ -106,110 +107,91 @@ public class MainActivity extends Activity {
     }
     */
 
-    //Custom low pass filter to smooth input to output
-    protected float lowPass( Float input, Float output ) {
-        if ( output == null ) return input;
-        output = output + ALPHA * (input - output);
-        return output;
-    }
+    private void changeVolumeLevel(float azimuth) {
 
-    //Scale the range 0-2pi to 0-1 for setting the player volumes
-    //This equation is simplified down
-    public Float scale(Float azimuth, Float min, Float max){
-
-        return (((azimuth - min))/(max-min));
-    }
-
-    //Using the scaled volume, set the appropriate player's volumes
-    public void setNorthEast(Float scaledVol){
-        NorthPlayer.setVolume(1-scaledVol,1-scaledVol);
-        EastPlayer.setVolume(scaledVol, scaledVol);
-        SouthPlayer.setVolume(0,0);
-        WestPlayer.setVolume(0, 0);
-
-        northText.setText("North: "+Float.toString(1-scaledVol));
-        eastText.setText("East: "+Float.toString(scaledVol));
-        southText.setText("South: 0");
-        westText.setText("West: 0");
-
-
-    }
-
-    public void setSouthEast(Float scaledVol){
-        NorthPlayer.setVolume(0,0);
-        EastPlayer.setVolume(1-scaledVol,1-scaledVol);
-        SouthPlayer.setVolume(scaledVol, scaledVol);
-        WestPlayer.setVolume(0,0);
-
-        northText.setText("North: 0");
-        eastText.setText("East: "+Float.toString(1-scaledVol));
-        southText.setText("South: "+Float.toString(scaledVol));
-        westText.setText("West: 0");
-    }
-
-    public void setSouthWest(Float scaledVol){
-        NorthPlayer.setVolume(0,0);
-        EastPlayer.setVolume(0,0);
-        SouthPlayer.setVolume(1-scaledVol,1-scaledVol);
-        WestPlayer.setVolume(scaledVol, scaledVol);
-
-        northText.setText("North: 0");
-        eastText.setText("East: 0");
-        southText.setText("South: "+Float.toString(1-scaledVol));
-        westText.setText("West: "+Float.toString(scaledVol));
-    }
-
-    public void setNorthWest(Float scaledVol){
-        NorthPlayer.setVolume(scaledVol, scaledVol);
-        EastPlayer.setVolume(0,0);
-        SouthPlayer.setVolume(0,0);
-        WestPlayer.setVolume(1-scaledVol,1-scaledVol);
-
-        northText.setText("North: "+Float.toString(scaledVol));
-        eastText.setText("East: 0");
-        southText.setText("South: 0");
-        westText.setText("West: "+Float.toString(1-scaledVol));
-    }
-
-
-
-    // Event listener for compass sensor.
-    //On change method implements smoothing with lopass filter and converts from radian to deg
-    private SensorEventListener mySensorEventListener = new SensorEventListener() {
-
-        @Override
-        public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        float angle;
+        if(azimuth <= 360 && azimuth > 270) {
+            angle = (azimuth - 270)/90;
+            NorthPlayer.setVolume(angle, angle);
+            angle = 1 - angle;
+            WestPlayer.setVolume(angle, angle);
         }
+        if(azimuth <= 270 && azimuth > 180) {
+            angle = (azimuth - 180)/90;
+            WestPlayer.setVolume(angle, angle);
+            angle = 1 - angle;
+            SouthPlayer.setVolume(angle, angle);
+        }
+        if(azimuth <= 180 && azimuth > 90) {
+            angle = (azimuth - 90)/90;
+            SouthPlayer.setVolume(angle, angle);
+            angle = 1 - angle;
+            EastPlayer.setVolume(angle, angle);
+        }
+        if(azimuth <= 90 && azimuth > 0) {
+            angle = azimuth/90;
+            EastPlayer.setVolume(angle, angle);
+            angle = 1 - angle;
+            NorthPlayer.setVolume(angle, angle);
+        }
+    }
 
-        @Override
-        public void onSensorChanged(SensorEvent event) {
-            // angle between the magnetic north direction
-            // 0=North, 90=East, 180=South, 270=West
-            azimuth = lowPass(event.values[0], azimuth);
+    private void releaseMediaResources() {
+       releasePlayer(NorthPlayer);
+       releasePlayer(EastPlayer);
+       releasePlayer(SouthPlayer);
+       releasePlayer(WestPlayer);
+    }
 
-            if(azimuth >= 0 || azimuth < (pi/2)){
+    private void releasePlayer(MediaPlayer mp) {
+        if(mp != null) {
+            mp.release();
+            mp = null;
+        }
+    }
 
-                setNorthEast(scale(azimuth, 0f, (pi/2)));
-            }
+    private void startMediaPlayers() {
+        startPlayer(NorthPlayer);
+        startPlayer(EastPlayer);
+        startPlayer(SouthPlayer);
+        startPlayer(WestPlayer);
+    }
 
-            if(azimuth >= (pi/2) || azimuth < pi){
+    private void startPlayer(MediaPlayer mp) {
+        mp.start();
+        mp.setVolume(0, 0);
+        //repeat song when it's stop
+        mp.setLooping(true);
+    }
 
-                setSouthEast(scale(azimuth, (pi/2), pi));
-            }
 
-            if(azimuth >= pi || azimuth < ((3*pi)/2)){
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()){
+            case R.id.settings_button:
+                //startSettingsActivity();
+                break;
 
-                setSouthWest((scale(azimuth, pi, (3*pi)/2)));
-            }
-
-            if(azimuth >= ((3*pi)/2) || azimuth < (2*pi)){
-
-                setNorthWest((scale(azimuth, (3*pi/2), (2*pi))));
-            }
-
+            case R.id.exit_button:
+                android.os.Process.killProcess(android.os.Process.myPid());
+                break;
 
         }
+    }
+
+    @Override
+    public void onSensorChanged(SensorEvent event) {
+        // angle between the magnetic north direction
+        // 0=North, 90=East, 180=South, 270=West
+        float azimuth = event.values[0];
+        compassView.updateData(azimuth);
+        Log.i("Angle Direction", String.valueOf(azimuth));
+        changeVolumeLevel(azimuth);
+    }
 
 
-    };
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+        //not in use
+    }
 }
